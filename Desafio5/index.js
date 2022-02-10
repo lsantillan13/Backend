76d3425644d5815f48fc1db0b4af09949faf63da
Lautaro <lautarosantillann@icloud.com>
2022-02-09 23:14:46 -0300

    feat (front/back) - Socket-io Chat

--- Desafio5/index.js
+++ Desafio5/index.js
@@ -1,64 +1,72 @@
-//------------------------------------------------------------------------- Imports -------------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ if (process.env.NODE_ENV !== 'production')                                                                                                                        /**/
-/**/  {require('dotenv').config()}                                                                                                                                     /**/
-/**/ const express = require('express');                                                                                                                               /**/
-/**/ const mongoose = require('mongoose');                                                                                                                             /**/
-/**/ const cors = require('cors');                                                                                                                                     /**/
-/**/ const multer = require('multer');                                                                                                                                 /**/
-/**/ const morgan = require('morgan');                                                                                                                                 /**/
-/**/ const path = require('path');                                                                                                                                     /**/
-/**/ const fs = require('fs');                                                                                                                                         /**/
-/**/ //const Container = require('./controllers/product.controllers.js');                                                                                                /**/
-/**/ const exphbs = require('express-handlebars');                                                                                                                     /**/
-/**/ const bodyParser = require('body-parser');                                                                                                                        /**/
-/**/                                                                                                                                                                   /**/
-//-------------------------------------------------------------------------  Models  ------------------------------------------------------------------------------------\\
-/**/ //const Products = require('./models/products');                                                                                                                  /**/
-//----------------------------------------------------------------------- Initialization --------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ const app = express();                                                                                                                                            /**/
-/**/ //require('./database')                                                                                                                                           /**/
-/**/                                                                                                                                                                   /**/
-//------------------------------------------------------------------------ Middlewares ----------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ app.use(express.json());                                                                                                                                          /**/
-/**/ app.use(bodyParser.urlencoded({extended: true}));                                                                                                                 /**/
-/**/ app.set('json spaces', 8);                                                                                                                                        /**/
-/**/ app.use(cors());                                                                                                                                                  /**/
-/**/ app.use(morgan('dev'));                                                                                                                                           /**/
-/**/                                                                                                                                                                   /**/
-//------------------------------------------------------------------------ Static files ---------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ //app.use(express.static(path.join(__dirname, 'public')));                                                                                                        /**/
-/**/ app.set('views', path.join(__dirname, 'views'));                                                                                                                  /**/    
-/**/ app.engine('hbs', exphbs.engine({                                                                                                                                 /**/
-/**/    defaultLayout: "main",                                                                                                                                         /**/
-/**/    layoutsDir: path.join(app.get('views'), '/layouts'),                                                                                                           /**/
-/**/    partialsdir: path.join(app.get('views'), '/partials'),                                                                                                         /**/
-/**/    extname: ".hbs",                                                                                                                                               /**/
-/**/ }));                                                                                                                                                              /**/
-/**/ app.set('view engine', 'hbs');                                                                                                                                    /**/
-/**/ app.use(express.static("public"));                                                                                                                                /**/
-/**/                                                                                                                                                                   /**/
-//-------------------------------------------------------------------------- Routes --------------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ const productsRoutes = require('./Router/products.routes');                                                                                                       /**/
-/**/ // API                                                                                                                                                            /**/
-/**/ app.use('/api', productsRoutes);                                                                                                                                  /**/
-/**/ app.get('/formulario');                                                                                                                                           /**/
-/**/ // Client                                                                                                                                                         /**/
-/**/ app.get('/', ( req, res ) => { res.render('index')});                                                                                                             /**/
-/**/ app.get('/productos', ( req, res) => {res.render('products')});                                                                                                   /**/
-/**/                                                                                                                                                                   /**/
-//------------------------------------------------------------------------ Server up --------------------------------------------------------------------------------------\\
-/**/                                                                                                                                                                   /**/
-/**/ app.set('port', process.env.PORT || 3000);                                                                                                                        /**/
-/**/ app.listen(app.get('port'), () => {console.log('Server is running on port', app.get('port')); });                                                                 /**/
-/**/                                                                                                                                                                   /**/
-/**/                                                                                                                                                                   /**/
-//------------------------------------------------------------------------ Server up -------------------------------------------------------------------------------------\\
-//------------------------------------------------------------------------ Server up -------------------------------------------------------------------------------------\\
-//------------------------------------------------------------------------ Server up -------------------------------------------------------------------------------------\\
-//------------------------------------------------------------------------ Server up -------------------------------------------------------------------------------------\\
-//------------------------------------------------------------------------ Server up -------------------------------------------------------------------------------------\\
+//---------------------------- Requiring stuff ------------------------------//
+/*                                                                           */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+/*const mongoose = require('mongoose');
+const multer = require('multer');
+/*                                                                           */
+//-------------------------------- Models -----------------------------------//
+/*                                                                           */
+//const Products = require('./models/products');
+/*                                                                          */
+//--------------------------- Initialization --------------------------------//
+/*                                                                          */
+const app = express();
+//----------------------------- Middlewares ---------------------------------//
+/*                                                                          */
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('json spaces', 8);
+app.use(cors());
+app.use(morgan('dev'));
+/*                                                                          */
+//----------------------------- Static Files -------------------------------//
+/*                                                                          */
+//app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  'hbs',
+  exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), '/layouts'),
+    partialsdir: path.join(app.get('views'), '/partials'),
+    extname: '.hbs',
+  })
+);
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+/*                                                                          */
+//--------------------------------- Routes ---------------------------------//
+/*                                                                          */
+const productsRoutes = require('./Router/products.routes');
+
+//============ API Routes ===========//
+/*                                   */
+app.use('/api', productsRoutes);
+app.get('/formulario');
+//=========  Client Routes  =========//
+/*                                   */
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/productos', (req, res) => {
+  res.render('products');
+});
+/*                                   */
+/*                                                                          */
+//--------------------------------- Server ---------------------------------//
+/*                                                                          */
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('Server is running on port', app.get('port'));
+});
+/*                                                                          */
+//--------------------------------- Others ---------------------------------//
+/*                                                                          */
---
+++ Desafio6/public/js/index.js
@@ -0,0 +1,104 @@
+window.onload = function(){ // => When document is ready...
+  let socket = io.connect();   
+
+  /*===== Formulary Handling =====*/
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const config = { header: {'Content-Type': 'application/json'},};
+    try{
+      let title = document.querySelector('#title').value;
+      let price = document.querySelector('#price').value;
+      let img = document.querySelector('#img').value;
+      await axios.post('http://localhost:8080/api/productos', { title, price, img }, config);
+      setTimeout(() => {
+        document.querySelector('#title').value = '';
+        document.querySelector('#price').value = '';
+        document.querySelector('#img').value = '';
+      }, 1500);
+    }catch(error){ console.log(error);}
+  };
+  document.getElementById('form').addEventListener('submit', handleSubmit);
+
+  /*====== Products Table Handling ======*/
+  /*Creation*/
+  const tb = document.createElement('tbody');
+  const tdName = document.createElement('td');
+  const tdPrice = document.createElement('td');
+  const tdPicture= document.createElement('td');
+  
+  /* DOM Manipulation */
+  const formBtn = document.getElementById('btn');
+  const table = document.getElementById('myTable');
+  /*Event Handler*/
+  formBtn.addEventListener('click', function () {
+    let title = document.querySelector('#title').value;
+    let price = document.querySelector('#price').value;
+    let img = document.querySelector('#img').value;
+    socket.emit('products:send', (product) = {
+      title,
+      price,
+      img
+    });
+  });
+
+  /*===== Fecha =====*/
+  let newDate = () => {return new Date();};
+
+  let getTheDate = () => {
+    /*Time*/
+    let hours = newDate().getHours().toString().length < 2 ? `0${+ newDate().getHours}`: newDate().getHours();
+    let minutes = newDate().getMinutes().toString().length < 2 ? `0${+ newDate().getMinutes()}` : newDate().getMinutes();
+    let secs =  newDate().getSeconds().toString().length < 2 ? `0${+ newDate().getSeconds()}` :  newDate().getSeconds();
+    let mainTime = `${hours}:${minutes}:${secs}`;
+    /*Date*/
+    let month = newDate().getMonth();
+    let days = newDate().getDate();
+    let year = newDate().getFullYear();
+    let mainDate = `${days}/${month}/${year}`;
+    let theWholeThing = ` [${mainDate} ${mainTime}]: `;
+    return theWholeThing;
+  };
+
+  socket.on('products:send', function (data){
+    /*Inner*/
+    tdName.innerHTML += `<p class="parrafo"> ${data.title} </p>`;
+    tdPrice.innerHTML += `<p class="parrafo"> ${data.price}</p´>`;
+    tdPicture.innerHTML += `<p class="parrafo"> <img src="${data.img}" alt="image_of_${data.title}" class="product_image"/> </p>`;
+    for (product in data){
+      const tr = document.createElement('tr');
+      /*Append*/
+      tb.appendChild(tr);
+      tr.appendChild(tdName);
+      tr.appendChild(tdPrice);
+      tr.appendChild(tdPicture);
+      table.appendChild(tb);            
+    }
+  });
+  /*===== SocketIO Handling =====*/
+  
+  let message = document.getElementById('message');
+  let username = document.getElementById('username');
+  let output = document.getElementById('output');
+  let actions = document.getElementById('actions');
+  let btn = document.getElementById('send');
+
+  btn.addEventListener('click', function() {
+    socket.emit('chat:message', {
+      message: message.value,
+      username: username.value
+    });
+  });
+
+  message.addEventListener('keypress', function(){
+    socket.emit('chat:typing', username.value);
+  });
+
+  socket.on('chat:message', function(data){
+    actions.innerHTML = '';
+    output.innerHTML += `<p> <strong>${data.username}</strong> <i class="notItalic">${getTheDate()}</i> : <i>${data.message}</i> </p>`;
+  });
+
+  socket.on('chat:typing', function(data){
+    actions.innerHTML = `<p><em>${data} está escribiendo...</em></p>`;
+  });
+};
